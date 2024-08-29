be94ceb8b09e2198143ce175f9f32ae1b6170c35
Pride <cpride@una.edu>
2024-08-29 13:25:47 -0500

    new files and folders

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello Node from Express on local dev box')
-})
-
-app.listen(5000)
+app.use(express.static('./'))
 
 console.log("I'm on a node server");
+
+app.get('/', function (req, res) {
+  //res.send('Hello Node from Express on local dev box')
+  res.sendFile('index.html')
+
+app.listen(5000)
+})
