7f5b9f6da2e17efdd5f50dd095db569839129ee1
Pride <cpride@una.edu>
2024-08-29 13:43:26 -0500

    got ejs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.static('./public'))
+
 app.use(express.static('./'))
 
 console.log("I'm on a node server");
@@ -8,6 +11,14 @@ console.log("I'm on a node server");
 app.get('/', function (req, res) {
   //res.send('Hello Node from Express on local dev box')
   res.sendFile('index.html')
+})
+
+app.get('/ejs', (req,res) => {
+  res.render('index', {
+    myServerVariable : "something from server"
+  });  
+
+  // can you get content from client to console
+})
 
 app.listen(5000)
-})
