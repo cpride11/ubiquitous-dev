0fdf84075afd54e8f48b8eea5f951fec2595c0be
Courtney <cpride@una.edu>
2024-09-05 12:34:48 -0500

    app.js

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
 const express = require('express')
 const app = express()
-
 app.set('view engine', 'ejs')
-app.use(express.static('./public'))
+app.use(express.static('./public/'))
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.ENV.uri; 
 
-app.use(express.static('./'))
+console.log(uri);
 
-console.log("I'm on a node server");
+// console.log('im on a node server change that and that tanad f, yo');
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', function (req, res) {
-  //res.send('Hello Node from Express on local dev box')
-  res.sendFile('index.html')
+  // res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html');
 })
 
-app.get('/ejs', (req,res) => {
+app.get('/ejs', (req,res)=>{
+``
   res.render('index', {
     myServerVariable : "something from server"
-  });  
+  });
 
-  // can you get content from client to console
+  //can you get content from client...to console? 
 })
 
 app.listen(5000)
