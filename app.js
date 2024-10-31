d89412c1e254fe2608ba54a422592326d4a357d8
Pride <cpride@una.edu>
2024-10-31 13:51:30 -0500

    update ejs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
 const { urlencoded } = require('body-parser')
 const { ObjectId } = require('mongodb')
@@ -16,7 +17,7 @@ app.use(express.static('./public/'))
 
 console.log(uri);
 
-console.log('im on a node server change that and that tanad f, yo');
+console.log('im on a node server, yo');
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,41 +29,64 @@ const client = new MongoClient(uri, {
   }
 });
 
-app.get('/', function (req, res) {
-  // res.send('Hello Node from Ex on local dev box')
-  res.render('song', {
-    songData : "Welcome to the song page"
-  });
-})
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
-app.get('/ejs', (req,res)=>{
+// app.get('/', function (req, res) {
+//   // res.send('Hello Node from Ex on local dev box')
+//   res.render('song', {
+//     songData : "Welcome to the song page"
+//   });
+// })
 
-  res.render('index', {
-    songData : "Welcome to the song page"
-  });
+// app.get('/ejs', (req,res)=>{
 
-  //can you get content from client...to console? 
-})
+//   res.render('index', {
+//     songData : "Welcome to the song page"
+//   });
+
+//   //can you get content from client...to console? 
+// })
 
 
-app.get('/test', async (req, res) => {
+app.get('/', async function (req, res) {
 
   //start here, delete old / and rename this one from /test to / 
   console.log('in /');
   await client.connect();
+  console.log("I should be connected");
 
-  console.log('I should be connected');
-  // Send a ping to confirm a successful connection
+  // p.ping("https://ubiquitous-umbrella-dev-idx5.onrender.com")
+  //   .then(result => {
+  //     console.log('I should be connected' + result);
+  //     // Send a ping to confirm a successful connection
+  //   })
+  //   .catch(result => {
+  //     console.error('Not connected' + result);
+  //   })
 
   let result = await client.db("courtneys-db").collection("courtneys-collection")
     .find({}).toArray();
   console.log(result);
 
+  // app.get('/ejs', (req, res) => {
   res.render('song', {
     songData: result
   });
-  //makes ure on song.ejs to change severVarable to songData
-})
+  // });
+  //makes sure on song.ejs to change severVarable to songData
+});
 
 app.post('/insert', async (req, res) => {
 
@@ -105,7 +129,7 @@ app.post('/delete/:id', async (req, res) => {
 
   console.log("in delete, req.parms.id: ", req.params.id)
 
-  client.connect;
+  await client.connect;
 
   const collection = client.db("courtneys-db").collection("courtneys-collection");
 
